6488409df39b249db973bf10926ef7b3c7fa65dd
Abhishek Raj Singh <abhishekrajsingh2509@gmail.com>
2024-11-08 19:32:04 +0530

    MERN Chat App

---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import authRoutes from "./routes/AuthRoutes.js";
+import contactsRoutes from "./routes/ContactRoutes.js";
+import setupSocket from "./socket.js";
+import MessagesRoutes from "./routes/MessagesRoutes.js";
+import channelRoutes from "./routes/ChannelRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+const corsConfig = {
+  origin: process.env.ORIGIN,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+};
+
+app.options("", cors(corsConfig));
+app.use(cors(corsConfig));
+
+app.use("/uploads/profiles", express.static("uploads/profiles"));
+app.use("/uploads/files", express.static("uploads/files"));
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactsRoutes);
+app.use("/api/messages", MessagesRoutes);
+app.use("/api/channel", channelRoutes);
+
+const server = app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+setupSocket(server);
+
+mongoose
+  .connect(databaseURL)
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => console.log(err.message));
